5a7a584dfd9b970804deee6165f1a6f65fa572f3
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2023-07-31 20:42:59 +0900

    Section/3 (#2)
    complete 3 chapter

---
+++ chapter_3/src/3-4/example.rs
@@ -0,0 +1,12 @@
+//this is annotation example section
+
+//annotation example one
+fn annotation_example_one() {
+    let lucky_number = 7; // I’m feeling lucky today.
+}
+
+//annotation example two
+fn annotation_example_two() {
+    // I’m feeling lucky today.
+    let lucky_number = 7;
+}
